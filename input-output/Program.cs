9b65f3b45abd0b2c27540626e1686edead3d2374
paulvenom <paul.c.fuller353@gmail.com>
2026-07-17 11:39:40 -0400

    C# Practice Project 4

---
+++ input-output/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
+
+        Console.WriteLine($"My name is {name}.");
+
+        Console.WriteLine($"My age is {args[0]}.");
+
+        Console.WriteLine("Program complete! Press any key to exit.");
+
+        Console.ReadKey();
+    }
+}
